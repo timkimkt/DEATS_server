2d56df3dd39cf6bd53ebdc8bd79e55b6a605c237
Edmund Aduse Poku <edmundapoku@github.com>
2022-02-18 12:41:54 -0500

    Enable app to run alongside app_json

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-#from tests.flask.app import app
+from tests.flask.app import app
 from tests.flask.json_test import app_json
  
 if __name__ == "__main__":
-        #app.run()
+        app.run()
         app_json.run()
