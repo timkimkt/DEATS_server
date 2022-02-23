7bf78188831a4f673466c3caffd7f80a1da2c31c
Edmund Aduse Poku <edmundapoku@github.com>
2022-02-22 23:52:31 -0500

    Process and save user requests on the server

--- tests/flask/app.py
+++ tests/flask/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import request
+from bson.objectid import ObjectId
 
 # from markupsafe import escape
 
 import os
 import pymongo
+import tests.flask.user_json as user_json
 
 DATABASE_URL = f'mongodb+srv://db:db@cluster0.ixijz.mongodb.net/?retryWrites=true&w=majority'
 
@@ -17,10 +19,12 @@ app = Flask(__name__)
 @app.route("/create_acc/", methods=['POST'])
 def create_account():
     data = request.get_json()
-    print("data", data)
-    print(request.headers['Content-Type'])
-    result = db.users.insert_one(data)
-    return str(result.inserted_id)
+
+    if data:
+        print("data", data)
+        print(request.headers['Content-Type'])
+        result = db.users.insert_one(user_json.create_user_json(data["email"], data["password"]))
+        return str(result.inserted_id)
 
 @app.route('/login/', methods=['POST'])
 def login():
@@ -47,9 +51,29 @@ def login():
 def show_users():
     data = request.get_json()
     print("data", data)
-    print(request.headers['Content-Type'])
+    #print(request.headers['Content-Type'])
     cursor = db.users.find({})
     return str(list(cursor))
-    
 
+@app.route("/request_del/", methods=['POST'])
+def request_delivery():
+    data = request.get_json()
+
+    if data:
+        print("data", data)
+        print(request.headers['Content-Type'])
+        result = db.users.update_one({"_id": ObjectId(data["id"])},
+            {"$set": user_json.request_delivery_json(data["fin_location"], data["res_location"])},)
+        return str(result.modified_count)
+
+@app.route("/start_del/", methods=['POST'])
+def start_delivery():
+    data = request.get_json()
+
+    if data:
+        print("data", data)
+        print(request.headers['Content-Type'])
+        result = db.users.update_one({"_id": ObjectId(data["id"])},
+            {"$set": user_json.start_delivery_json(data["fin_location"])},)
+        return str(result.modified_count)
     
---
+++ tests/flask/user_json.py
@@ -0,0 +1,33 @@
+def create_user_json(email, password):
+    return {
+            "email" : email,
+            "password" : password,
+            "user_type" : None,
+            "active" : False,
+            "matched" : False,
+            "fin_location" : {
+                "x" : None, 
+                "y" : None
+            },
+            "res_location" : {
+                "x" : None, 
+                "y" : None
+            }
+        }
+
+def request_delivery_json(fin_location, res_location):
+    return {
+            "user_type" : 'C',
+            "active" : True,
+            "fin_location" : fin_location,
+            "res_location" : res_location
+        }
+
+# temporarily assume final location and current location are the same
+def start_delivery_json(fin_location):
+    return {
+            "user_type" : 'D',
+            "active" : True,
+            "fin_location" : fin_location
+        }
+
