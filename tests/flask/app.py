2775a5944a702418ce4b45857150c5409b86344a
Edmund Aduse Poku <edmundapoku@github.com>
2022-02-28 04:57:51 -0500

    Added update_acc functionality to server interface; Established backend Dartmouth email validation and strong password check for user account creation and login

--- tests/flask/app.py
+++ tests/flask/app.py
@@ -1,21 +1,18 @@
-from flask import Flask, jsonify, request
-from bson.objectid import ObjectId
-from logic.customer_finder import CustomerFinder
-
-# from markupsafe import escape
-
-import os
 import pymongo
 import tests.flask.user_json as user_json
 
-DATABASE_URL = f'mongodb+srv://db:db@cluster0.ixijz.mongodb.net/?retryWrites=true&w=majority'
+from bson.objectid import ObjectId
+from flask import Flask, request
+from logic.customer_finder import CustomerFinder
+from tests.flask.helper_functions import validate_password
+from tests.flask.validate_email import validate_email
 
+DATABASE_URL = f'mongodb+srv://db:db@cluster0.ixijz.mongodb.net/?retryWrites=true&w=majority'
 client = pymongo.MongoClient(DATABASE_URL)
 db = client.test
-
 app = Flask(__name__)
-
 g_count = 0
+
 
 @app.route("/create_acc/", methods=['POST'])
 def create_account():
@@ -24,9 +21,51 @@ def create_account():
     if data:
         print("data", data)
         print(request.headers['Content-Type'])
-        result = db.users.insert_one(user_json.create_user_json(data["email"], data["password"], data["name"], data["phone_num"]))
-        msg = "User deets are now on the server"
-        return user_json.create_acc_response_json(True, msg, str(result.inserted_id))
+
+        try:
+            if data["email"]:
+                valid_email = validate_email(data["email"])
+
+                if data["password"]:
+                    # strong password creation is a pain, so allow developers to test without password validation
+                    if not data["test"]:
+                        validate_password(data["password"])
+                    result = db.users.insert_one(
+                        user_json.create_user_json(valid_email.email, data["password"], data.get("name"),
+                                                   data.get("phone_num")))
+                    msg = "User deets are now on the server"
+                    return user_json.create_acc_response_json(True, msg, str(result.inserted_id))
+
+        except ValueError as err:
+            return user_json.create_acc_response_json(False, str(err))
+
+
+@app.route("/update_acc/", methods=['POST'])
+def update_account():
+    data = request.get_json()
+    msg = "Absent JSON data. Provide a valid JSON data"
+    succeeded = False
+
+    if data:
+        if data["id"]:
+            user_id = data["id"]
+            del data["id"]
+            succeeded = bool(db.users.update_one({"_id": ObjectId(user_id)},
+                                                 {"$set": data}).modified_count)
+
+            if succeeded:
+                msg = "The user's info has been updated successfully"
+
+            else:
+                msg = "The user's info wasn't updated. The change already exist"
+
+            print("modified: ", succeeded, " number of users")
+
+        else:
+            msg = "No user id provided. The server needs to know the id of the user who's info you want to update"
+
+    return user_json.success_response_json(succeeded, msg)
+
 
 @app.route("/delete_acc/", methods=['POST'])
 def delete_account():
@@ -40,6 +79,7 @@ def delete_account():
         msg = "User with id, " + data["id"] + ", has been removed from the server"
         return user_json.delete_acc_response_json(True, msg)
 
+
 @app.route('/login/', methods=['POST'])
 def login():
     data = request.get_json()
@@ -51,28 +91,34 @@ def login():
     id = None
     name = None
     phone_num = None
-    
-    if data:
-        user = db.users.find_one({"email" : data["email"]})
-        print("email_check", user)          
-        if not user:
-            msg = "The email provided does not exist on the server"
 
-        else:
-            user = db.users.find_one({"email" : data["email"], "password" : data["password"]})
-            print("password_check", user)
+    try:
+        if data["email"]:
+            validate_email(data["email"])
+            user = db.users.find_one({"email": data["email"]})
+            print("email_check", user)
             if not user:
-                msg = "Email exists but the password provided doesn't match what's on the server" 
+                msg = "The Dartmouth email provided does not exist on the server"
 
             else:
-                print(user)
-                succeeded = True
-                msg = "Yayy, the user exists!"
-                id = str(user["_id"])
-                name = user["name"]
-                phone_num = user["phone_num"]
+                user = db.users.find_one({"email": data["email"], "password": data["password"]})
+                print("password_check", user)
+                if not user:
+                    msg = "The provided Dartmouth email exists but the password doesn't match what's on the server"
 
-    return user_json.login_response_json(succeeded, msg, id, name, phone_num)
+                else:
+                    print(user)
+                    succeeded = True
+                    msg = "Yayy, the user exists!"
+                    id = str(user["_id"])
+                    name = user["name"]
+                    phone_num = user["phone_num"]
+
+        return user_json.login_response_json(succeeded, msg, id, name, phone_num)
+
+    except ValueError as err:
+        return user_json.create_acc_response_json(False, str(err))
+
 
 @app.route("/show_users/", methods=['GET'])
 def show_users():
@@ -81,11 +127,13 @@ def show_users():
     cursor = db.users.find({})
     return user_json.show_users_response_json(str(list(cursor)))
 
+
 @app.route("/global_count/", methods=['GET'])
 def global_count():
     global g_count
     g_count += 1
     return user_json.global_count_response_json(g_count)
+
 
 @app.route("/request_del/", methods=['POST'])
 def request_delivery():
@@ -95,14 +143,16 @@ def request_delivery():
         print("data", data)
         print(request.headers['Content-Type'])
         result = db.users.update_one({"_id": ObjectId(data["id"])},
-            {"$set": user_json.request_delivery_json(data["fin_location"], data["res_location"])},)
+                                     {"$set": user_json.request_delivery_json(data["fin_location"],
+                                                                              data["res_location"])}, )
         print("modified: ", result.modified_count, " number of customers")
 
         # ensure one-to-one delivery-customer matching
-        cursor = db.users.find({"user_type": "D", "active": True, "matched": None})
+        # cursor = db.users.find({"user_type": "D", "active": True, "matched": None})
 
-        return user_json.success_response_json(bool(result.modified_count))
-        
+        return user_json.success_response_json(bool(result.modified_count), "Delivery requested")
+
+
 @app.route("/start_del/", methods=['POST'])
 def start_delivery():
     data = request.get_json()
@@ -111,39 +161,60 @@ def start_delivery():
         print("data", data)
         print(request.headers['Content-Type'])
         result = db.users.update_one({"_id": ObjectId(data["id"])},
-            {"$set": user_json.start_delivery_json(data["fin_location"])},)
+                                     {"$set": user_json.start_delivery_json(data["fin_location"])}, )
         print("modified: ", result.modified_count, " number of deliverers")
-        
+
         customer_finder = CustomerFinder(data["fin_location"],
-            db.users.find({"user_type": "C", "active": True, "matched": None}))
+                                         db.users.find({"user_type": "C", "active": True, "matched": None}))
 
         customer_finder.sort_customers()
-        
+
         return user_json.start_delivery_response_json(customer_finder.get_k_least_score_customers(data["num"]))
+
 
 @app.route("/match/", methods=['POST'])
 def match():
     data = request.get_json()
+    msg = "Absent JSON data. Provide a valid JSON data"
+    succeeded = False
 
     if data:
         print("data", data)
         print(request.headers['Content-Type'])
-        result = db.users.update_one({"_id": ObjectId(data["matched_id"])},
-            {"$set": user_json.match_customer_json(data["id"])},)
-        print("modified: ", result.modified_count, " number of customers")
+        succeeded = db.users.update_one({"_id": ObjectId(data["matched_id"])},
+                                        {"$set": user_json.match_customer_json(data["id"])}, ).modified_count
+        print("modified: ", succeeded, " number of customers")
 
-        return user_json.success_response_json(bool(result.modified_count))
+        if succeeded:
+            msg = "Request completed. User has been matched"
+
+        else:
+            msg = "Request not completed. User has already been matched"
+
+        print("modified: ", succeeded, " number of users")
+
+    return user_json.success_response_json(succeeded, msg)
+
 
 @app.route("/unmatch/", methods=['POST'])
 def unmatch():
     data = request.get_json()
+    msg = "Absent JSON data. Provide a valid JSON data"
+    succeeded = False
 
     if data:
         print("data", data)
         print(request.headers['Content-Type'])
-        result = db.users.update_one({"_id": ObjectId(data["matched_id"])},
-            {"$set": user_json.match_customer_json(None)},)
-        print("modified: ", result.modified_count, " number of customers")
+        succeeded = db.users.update_one({"_id": ObjectId(data["matched_id"])},
+                                        {"$set": user_json.match_customer_json(None)}, ).modified_count
+        print("modified: ", succeeded, " number of customers")
 
-        return user_json.success_response_json(bool(result.modified_count))
-    
+        if succeeded:
+            msg = "Request completed. User has been unmatched"
+
+        else:
+            msg = "Request not completed. User has already been unmatched"
+
+        print("modified: ", succeeded, " number of users")
+
+    return user_json.success_response_json(succeeded, msg)
--- tests/flask/user_json.py
+++ tests/flask/user_json.py
@@ -1,87 +1,94 @@
 # ------------- Inputs for MongoDB ------------- #
 def create_user_json(email, password, name=None, phone_num=None):
     return {
-            "email" : email,
-            "name" : name,
-            "password" : password,
-            "phone_num" : phone_num,
-            "user_type" : None,
-            "active" : False,
-            "matched" : None,
-            "fin_location" : {
-                "x" : None, 
-                "y" : None
-            },
-            "res_location" : {
-                "x" : None, 
-                "y" : None
-            }
+        "email": email,
+        "name": name,
+        "password": password,
+        "phone_num": phone_num,
+        "user_type": None,
+        "active": False,
+        "matched": None,
+        "fin_location": {
+            "x": None,
+            "y": None
+        },
+        "res_location": {
+            "x": None,
+            "y": None
         }
+    }
+
 
 def request_delivery_json(fin_location, res_location):
     return {
-            "user_type" : 'C',
-            "active" : True,
-            "fin_location" : fin_location,
-            "res_location" : res_location
-        }
+        "user_type": 'C',
+        "active": True,
+        "fin_location": fin_location,
+        "res_location": res_location
+    }
+
 
 # temporarily assume final location and current location are the same
 def start_delivery_json(fin_location):
     return {
-            "user_type" : 'D',
-            "active" : True,
-            "fin_location" : fin_location
-        }
+        "user_type": 'D',
+        "active": True,
+        "fin_location": fin_location
+    }
+
 
 def match_customer_json(deliver_id):
     return {
-            "matched" : deliver_id
-        }
+        "matched": deliver_id
+    }
+
 
 # ------------- JSON responses ------------- #
 def login_response_json(succeeded, msg, id, name, phone_num):
     return {
-            "succeeded" : succeeded,
-            "msg" :msg,
-            "id" : id,
-            "name" : name,
-            "phone_num" : phone_num
-        }
+        "succeeded": succeeded,
+        "msg": msg,
+        "id": id,
+        "name": name,
+        "phone_num": phone_num
+    }
 
-def create_acc_response_json(succeeded, msg, user_id):
+
+def create_acc_response_json(succeeded, msg, user_id=None):
     return {
-            "succeeded" : succeeded,
-            "msg" : msg,
-            "user_id" :user_id,
-        }
+        "succeeded": succeeded,
+        "msg": msg,
+        "user_id": user_id,
+    }
+
 
 def delete_acc_response_json(succeeded, msg):
     return {
-            "succeeded" : succeeded,
-            "msg" : msg,
-        }
+        "succeeded": succeeded,
+        "msg": msg,
+    }
+
 
 def start_delivery_response_json(unmatched_customers):
     return {
-            "unmatched_customers" : unmatched_customers
-        }
+        "unmatched_customers": unmatched_customers
+    }
 
-def success_response_json(succeeded):
+
+def success_response_json(succeeded, msg):
     return {
-            "succeeded" : succeeded
-        }
+        "succeeded": succeeded,
+        "msg": msg
+    }
+
 
 def show_users_response_json(registered_users):
     return {
-            "registered_users" : registered_users
-        }
+        "registered_users": registered_users
+    }
+
 
 def global_count_response_json(global_count):
     return {
-            "global count" : global_count
-        }
-
-        
-
-
+        "global count": global_count
+    }
