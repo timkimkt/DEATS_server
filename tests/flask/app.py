bde7f6af748c8ef6b40e6bca7a1be0e5bf81e1a0
Edmund Aduse Poku <edmundapoku@github.com>
2022-03-06 19:54:34 -0500

    Update app and related files/functions to take in and store a 'drop_loc_name' key during orders to be returned to delivererers when they make delivery requests

--- tests/flask/app.py
+++ tests/flask/app.py
@@ -146,8 +146,8 @@ def order_delivery():
     if data:
         print("data", data)
         print(request.headers['Content-Type'])
-        result = db.orders.insert_one(user_json.order_delivery_json(data["id"], data["pickup_loc"],
-                                                                    data["drop_loc"], data["pickup_loc_name"]))
+        result = db.orders.insert_one(user_json.order_delivery_json(data["id"], data["pickup_loc"], data["drop_loc"],
+                                                                    data["pickup_loc_name"], data["drop_loc_name"]))
         print("modified: ", result.inserted_id, " number of customers")
 
         return user_json.order_delivery_response_json(bool(result.inserted_id), str(result.inserted_id))
