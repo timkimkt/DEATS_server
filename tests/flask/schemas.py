134f8b77feb6fa985891e0df49e5414ffa7087ac
Edmund Aduse Poku <edmundapoku@github.com>
2022-04-24 01:12:50 -0400

    Add flask-apispec and set up schemas for webargs

---
+++ tests/flask/schemas.py
@@ -0,0 +1,49 @@
+from marshmallow import Schema, fields
+
+
+class CreateAccSchema(Schema):
+    email = fields.Str(required=True)
+    password = fields.Str(required=True)
+
+
+class ManipulateAccSchema(Schema):
+    user_id = fields.Str(required=True)
+
+
+class LoginSchema(Schema):
+    email = fields.Str(required=True)
+    password = fields.Str(required=True)
+
+
+class OrderDelSchema(Schema):
+    user_id = fields.Int(required=True)
+    pickup_loc = fields.Str(required=True)
+    drop_loc = fields.Str(required=True)
+    GET_code = fields.DateTime(required=True)
+
+
+class UpdateOrderSchema(Schema):
+    user_id = fields.Int(required=True)
+    pickup_loc = fields.Str(required=True)
+    drop_loc = fields.Str(load_only=True)
+    GET_code = fields.DateTime(load_only=True)
+
+
+class MakeDelSchema(Schema):
+    user_id = fields.Str(required=True)
+    start_point = fields.Str(required=True)
+    destination = fields.Str(required=True)
+    num_deliveries = fields.Int(required=True)
+
+
+class MatchUnmatchOrderInfo(Schema):
+    user_id = fields.Str(required=True)
+    order_id = fields.Str(required=True)
+
+
+class OrdersDeliveriesSchema(Schema):
+    user_id = fields.Str(required=True)
+
+
+
+
