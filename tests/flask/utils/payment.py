ea5d6a59a99bdfc37e6f057468699509512dee4a
Edmund Aduse Poku <edmundapoku@github.com>
2022-05-18 15:45:13 -0400

    Extract location coords into a tuple for geodesic fxn in compute_token_fee

--- tests/flask/utils/payment.py
+++ tests/flask/utils/payment.py
@@ -8,8 +8,10 @@ COST_PER_DELTA_SURGE = 0.2
 
 
 def compute_token_fee(pickup_coordinates, drop_coordinates, num_unmatched_orders):
+    pickup_tuple = (pickup_coordinates["lat"], pickup_coordinates["long"])
+    drop_tuple = (drop_coordinates["lat"], pickup_coordinates["long"])
     base_fee = 0.1
-    distance = geodesic(pickup_coordinates, drop_coordinates).miles
+    distance = geodesic(pickup_tuple, drop_tuple).miles
     distance_fee = (distance // DELTA_RADIAL_DISTANCE) * COST_PER_DELTA_RADIAL_DISTANCE
     surge_fee = (num_unmatched_orders // DELTA_SURGE) * COST_PER_DELTA_SURGE
 
