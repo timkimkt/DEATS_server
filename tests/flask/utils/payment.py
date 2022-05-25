3baee33e240fc09c3ec67bf6bbf097106f2c021c
Edmund Aduse Poku <edmundapoku@github.com>
2022-05-24 22:47:23 -0400

    Increase base fee to  prevent invalid payments

--- tests/flask/utils/payment.py
+++ tests/flask/utils/payment.py
@@ -1,5 +1,7 @@
 from geopy.distance import geodesic
 
+BASE_FEE = 0.25
+
 DELTA_RADIAL_DISTANCE = 0.2  # in miles
 COST_PER_DELTA_RADIAL_DISTANCE = 0.25  # in DEATS tokens
 
@@ -12,7 +14,6 @@ COST_PER_TOKEN = 300  # $3
 def compute_token_fee(pickup_loc, drop_loc, num_unmatched_orders):
     pickup_tuple = (pickup_loc["coordinates"]["lat"], pickup_loc["coordinates"]["long"])
     drop_tuple = (drop_loc["coordinates"]["lat"], drop_loc["coordinates"]["long"])
-    base_fee = 0.1
     distance = geodesic(pickup_tuple, drop_tuple).miles
     print("pickup loc:", pickup_loc, "drop loc:", drop_loc, "distance:", distance)
 
@@ -22,7 +23,7 @@ def compute_token_fee(pickup_loc, drop_loc, num_unmatched_orders):
     surge_fee = (num_unmatched_orders // DELTA_SURGE) * COST_PER_DELTA_SURGE
     print("surge fee:", surge_fee)
 
-    return base_fee + distance_fee + surge_fee
+    return BASE_FEE + distance_fee + surge_fee
 
 
 def compute_new_fee(new_pickup_loc, new_drop_loc, old_pickup_loc, old_drop_loc, old_fee):
